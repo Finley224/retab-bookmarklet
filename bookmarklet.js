5361558da551dd2be781d310010938720949d2a7
Finley <161162126+xavier224@users.noreply.github.com>
2024-08-22 17:20:03 -0400

    Change the URL for the JS file

--- bookmarklet.js
+++ bookmarklet.js
@@ -21,8 +21,9 @@ javascript: (() => {
     // Insert the title protect script if it does not exist
     if (!document.getElementById('titleprotect')) {
         script = document.createElement('script');
-        script.setAttribute('src',"http://localhost:8000/protecttitle.js");
+        script.setAttribute('src',"https://raw.githubusercontent.com/xavier224/retab/47b950e73142eaade72637793c4794882a3745aa/protecttitle.js");
         script.setAttribute('id',"titleprotect");
+        // Append the script to the body of the HTML Doc
         document.body.appendChild(script);
     }
 
--- protecttitle.js
+++ protecttitle.js
@@ -1,9 +1,11 @@
+// Runs when a change is detected to the title of the html page, checks if a title is stored in session storage. If it can be found in session storage, set the title back to stop the website from modifiying it.
 changeDetect = new MutationObserver(function() {
         if (typeof window.sessionStorage.tabName != "undefined") {    
             if (document.title != window.sessionStorage.tabName) {
                 document.title = window.sessionStorage.tabName;
-                console.log("TitleProtect: Protected Title from changes")
        }
     }
 });
+
+// Start watching for changes to the title
 changeDetect.observe(document.querySelector('title'), {attributes: true, childList: true, subtree: true, characterData: true});
