07de57c73d240361531308d07139aa7b7b9013c7
Finley <161162126+Finley224@users.noreply.github.com>
2024-08-23 13:36:02 -0400

    Update URL for protect script
    I changed my username, therefore the URL needs to be changed to reflect that.

--- bookmarklet.js
+++ bookmarklet.js
@@ -21,7 +21,7 @@ javascript: (() => {
     // Insert the title protect script if it does not exist
     if (!document.getElementById('titleprotect')) {
         script = document.createElement('script');
-        script.setAttribute('src',"https://raw.githubusercontent.com/xavier224/retab/47b950e73142eaade72637793c4794882a3745aa/protecttitle.js");
+        script.setAttribute('src',"https://raw.githubusercontent.com/finley224/retab/47b950e73142eaade72637793c4794882a3745aa/protecttitle.js");
         script.setAttribute('id',"titleprotect");
         // Append the script to the body of the HTML Doc
         document.body.appendChild(script);
