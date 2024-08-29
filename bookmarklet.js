8ba02a087ee5000d4b11e582c28422e7f21c7691
Finley <161162126+Finley224@users.noreply.github.com>
2024-08-29 11:48:48 -0400

    Add update notifications

--- bookmarklet.js
+++ bookmarklet.js
@@ -13,5 +13,18 @@ javascript: (() => {
         document.head.appendChild(script);
     }
 
+    /* Check for updates */
+    version = "0.2"
+    channel = "mainChannelVersion"
+    fetch("https://raw.githubusercontent.com/Finley224/retab-bookmarklet/main/version.json", { method: "GET" }).then((response) => response.json()).then(
+        (json) => { if (json[channel] != version) 
+            {
+                alert("Bookmark is out of date, vist https://github.com/Finley224/retab-bookmarklet to get the latest version. Current channel is " + channel + ". Current Version is " + version + ".");
+            } 
+        }
+    );
+    console.log(serverVersion);
+
+    /* Run the script from github */
     fetch("https://raw.githubusercontent.com/Finley224/retab-bookmarklet/main/settabname.js", { method: "GET" }).then((response) => response.text()).then((text) => eval(text));
 })();
