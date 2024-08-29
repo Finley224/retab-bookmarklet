06598cda65ff9d89944eb7ff128ae22f8116b398
Finley <161162126+Finley224@users.noreply.github.com>
2024-08-29 11:16:27 -0400

    Revamp bookmarklet
    It is now working fully, it works even with a strict CORS policy, it can be updated on the fly.

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,13 +1,17 @@
 javascript: (() => {
-    // Insert the title protect script if it does not exist
+    /* Insert the title protect script if it does not exist */
     if (!document.getElementById('titleprotect')) {
         script = document.createElement('script');
-        script.setAttribute('src',"https://raw.githubusercontent.com/Finley224/retab-bookmarklet/main/protecttitle.js");
-        script.setAttribute('id',"titleprotect");
-        // Append the script to the body of the HTML Doc
-        document.body.appendChild(script);
+        script.setAttribute('id', "titleprotect");
+        /* grab the scirpt and set innerHTML to it, this is needed as CORS prevents using the src tag */
+        fetch("https://raw.githubusercontent.com/Finley224/retab-bookmarklet/main/protecttitle.js", { method: "GET" }).then(
+            (response) => response.text()
+        ).then(
+            (text) => script.innerHTML = text
+        );
+        /* Append the script to the body of the HTML Doc */
+        document.head.appendChild(script);
     }
 
-    setTabName();
-
-  })();
+    fetch("https://raw.githubusercontent.com/Finley224/retab-bookmarklet/main/settabname.js", { method: "GET" }).then((response) => response.text()).then((text) => eval(text));
+})();
--- protecttitle.js
+++ protecttitle.js
@@ -1,4 +1,4 @@
-// Runs when a change is detected to the title of the html page, checks if a title is stored in session storage. If it can be found in session storage, set the title back to stop the website from modifiying it.
+/* Runs when a change is detected to the title of the html page, checks if a title is stored in session storage. If it can be found in session storage, set the title back to stop the website from modifiying it. */
 changeDetect = new MutationObserver(function() {
         if (typeof window.sessionStorage.tabName != "undefined") {    
             if (document.title != window.sessionStorage.tabName) {
@@ -7,5 +7,5 @@ changeDetect = new MutationObserver(function() {
     }
 });
 
-// Start watching for changes to the title
+/* Start watching for changes to the title */
 changeDetect.observe(document.querySelector('title'), {attributes: true, childList: true, subtree: true, characterData: true});
