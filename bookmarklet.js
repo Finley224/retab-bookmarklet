d5b85dc09d3e063adfa589a709751924c48757bb
Finley <161162126+Finley224@users.noreply.github.com>
2024-08-29 10:03:52 -0400

    Move the tab rename code to the hosted file

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,23 +1,4 @@
 javascript: (() => {
-    // Check if the tab name is saved, if it is saved, offer the user to set it, if not, have the user enter a new title.
-    if (typeof window.sessionStorage.tabName != "undefined" ) {
-        sessionTitle = window.sessionStorage.tabName;
-        if (confirm(`Would you like to set the title to "${sessionTitle}"? \nCanceling will allow you to put in another name`)) {
-            newTitle = sessionTitle;
-        } else {
-            newTitle = prompt("Enter Tab Title:");
-        }
-
-    } else {
-        newTitle = prompt("Enter Tab Title:");
-    }
-    
-    // Save the title to sesion storage
-    window.sessionStorage.tabName = newTitle;
-
-    // Update the title
-    document.title = newTitle;
-
     // Insert the title protect script if it does not exist
     if (!document.getElementById('titleprotect')) {
         script = document.createElement('script');
@@ -26,5 +7,7 @@ javascript: (() => {
         // Append the script to the body of the HTML Doc
         document.body.appendChild(script);
     }
+
+    setTabName();
 
   })();
--- protecttitle.js
+++ protecttitle.js
@@ -9,3 +9,24 @@ changeDetect = new MutationObserver(function() {
 
 // Start watching for changes to the title
 changeDetect.observe(document.querySelector('title'), {attributes: true, childList: true, subtree: true, characterData: true});
+
+// Check if the tab name is saved, if it is saved, offer the user to set it, if not, have the user enter a new title.
+function setTabName() {
+    if (typeof window.sessionStorage.tabName != "undefined" ) {
+        sessionTitle = window.sessionStorage.tabName;
+        if (confirm(`Would you like to set the title to "${sessionTitle}"? \nCanceling will allow you to put in another name`)) {
+            newTitle = sessionTitle;
+        } else {
+            newTitle = prompt("Enter Tab Title:");
+        }
+
+    } else {
+        newTitle = prompt("Enter Tab Title:");
+    }
+    
+    // Save the title to sesion storage
+    window.sessionStorage.tabName = newTitle;
+
+    // Update the title
+    document.title = newTitle;
+}
