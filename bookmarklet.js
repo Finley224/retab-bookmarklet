709d981b1841d5f8b0ae7613da5da60d782ffbe6
Finley <161162126+Finley224@users.noreply.github.com>
2024-08-30 08:15:53 -0400

    Loader v1.0
    First main version of the loader, the whole script is not done and is still in beta. The update check has been modified to simply provide the current version to the script instead, which can do what it wants with that info.

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,18 +1,7 @@
 javascript: (() => {
-    /* Check for updates */
-    updateCheck = true;
-    serverVersion = "null";
-    version = "0.3";
-    channel = "mainChannelVersion";
-    if (updateCheck) {
-    fetch("https://raw.githubusercontent.com/Finley224/retab-bookmarklet/main/version.json", { method: "GET" }).then((response) => response.json()).then(
-        (json) => { if (json[channel] != version) 
-            {
-                alert("Bookmark is out of date, visit https://github.com/Finley224/retab-bookmarklet to get the latest version. Current channel is " + channel + ". Current Version is " + version + ". Server version is " + serverVersion + ".");
-            } 
-        }
-    );
-    }
+    /* Script info vars */
+    launcherVersion = "1.0";
+    launcherChannel = "mainChannelVersion";
 
     /* Run the script from github */
     fetch("https://raw.githubusercontent.com/Finley224/retab-bookmarklet/main/main.js", { method: "GET" }).then((response) => response.text()).then((text) => window.eval(text));
